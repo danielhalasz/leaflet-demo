5c0b58029b5b8503df87f3722df0fb1de5c453e3
Daniel Halasz <halaszdaniel@gmail.com>
2022-10-18 15:35:00 +0200

    map center moves to selected result

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedResult, setSelectedResult] = useState({ lat: 50, lon: 1 });
 
   useEffect(() => {
     fetch(
@@ -17,10 +18,17 @@ function App() {
       });
   }, [searchQuery]);
 
-  const handleClick = (e) => {
+  const handleSearchClick = (e) => {
     e.preventDefault();
     console.log(e.target.searchField.value);
     setSearchQuery(e.target.searchField.value);
+  };
+
+  const handleResultClick = (e) => {
+    const selection = searchResults.find(
+      (result) => result.display_name === e.target.innerText
+    );
+    setSelectedResult(selection);
   };
 
   const SetViewOnResult = ({ coords }) => {
@@ -31,21 +39,25 @@ function App() {
   };
 
   SetViewOnResult.defaultProps = {
-    coords: [51.505, -0.09],
+    coords: [50.0, 1.0],
   };
 
   return (
     <div>
-      <form onSubmit={handleClick}>
+      <form onSubmit={handleSearchClick}>
         <input type="text" name="searchField" />
         <button type="submit">Search</button>
       </form>
 
-      {searchResults.map((result) => (
-        <div key={result.place_id}>
-          <p>{result.display_name}</p>
-        </div>
-      ))}
+      <ul>
+        {searchResults.map((result) => (
+          <div>
+            <li key={result.place_id} onClick={handleResultClick}>
+              {result.display_name}
+            </li>
+          </div>
+        ))}
+      </ul>
 
       <MapContainer center={[50.0, 1.0]} zoom={6} scrollWheelZoom={false}>
         <TileLayer
@@ -53,9 +65,7 @@ function App() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {searchResults.length > 0 ? (
-          <SetViewOnResult
-            coords={[searchResults[0].lat, searchResults[0].lon]}
-          />
+          <SetViewOnResult coords={[selectedResult.lat, selectedResult.lon]} />
         ) : (
           <SetViewOnResult />
         )}
